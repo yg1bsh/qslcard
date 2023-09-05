8ae613e896a973f194e606d08ccf6361ab406fe4
agusdani <agusdani@gmail.com>
2023-09-06 01:32:23 +0700

    update verify qrcode

--- qslcard.js
+++ qslcard.js
@@ -1,1 +1,1 @@
-const apiUrl = 'https://script.google.com/macros/s/AKfycbzhrzuEosD7zJTwK5KXI-ioRG5rdVLK_yvkzqRdL1QK2DIVTDLujmLqg1C0atM9CNI/exec';
+const apiUrl = 'https://script.google.com/macros/s/AKfycbyk_ECJ2r5_dts8W6SycdzpwNVxALeC4IWQ7mNjYul8O35_4Ji3bL4b6RSi_q9UsLyN/exec';
