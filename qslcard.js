15efe570f11f6e715d9fa118b8aeca5980e90e42
agusdani <agusdani@gmail.com>
2024-03-21 00:13:26 +0700

    Revisi URL Api Service

--- qslcard.js
+++ qslcard.js
@@ -1,1 +1,5 @@
-const apiUrl = 'https://script.google.com/macros/s/AKfycbyk_ECJ2r5_dts8W6SycdzpwNVxALeC4IWQ7mNjYul8O35_4Ji3bL4b6RSi_q9UsLyN/exec';
+// const apiUrl = 'https://script.google.com/macros/s/AKfycbyk_ECJ2r5_dts8W6SycdzpwNVxALeC4IWQ7mNjYul8O35_4Ji3bL4b6RSi_q9UsLyN/exec';
+
+// const apiUrl = 'https://script.google.com/macros/s/AKfycbzVbXlOk6N6uCUSElaowjPUidlVeF29WwusqnW-3N5wLnTZj__tUHjGarngB9cTfLP-/exec';
+
+const apiUrl = 'https://script.google.com/macros/s/AKfycbxo_z53Ut1oMVQPSDVEUgDvC_5VHPAfqmyYZljBgG_mbhwU5bFLgZuIA9vCUuc9SdLO/exec';
